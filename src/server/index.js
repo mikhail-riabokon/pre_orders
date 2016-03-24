62af5a255a4d37b213507e69a47509c7a3b7bd3a
mikhail-riabokon <mikhail.riabokon@gmail.com>
2016-03-24 17:06:33 +0200

    express server

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const config = require('./config');
 const app = express();
-const port = 9999;
 
-app.listen(port, () => {
-  console.log('Server is started http://localhost:%s', port);
+app.listen(config.port, () => {
+  console.log('Server is started http://localhost:%s', config.port);
 });
