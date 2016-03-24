91c59bc962f0129e13aac4e70abf7a2e81875df8
mikhail-riabokon <mikhail.riabokon@gmail.com>
2016-03-24 18:01:54 +0200

    react babel config

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const config = require('./config');
+const renderInitialPage = require('./middlewares/renderInitialPage');
 const app = express();
+
+// if (process.env.HOT) {
+//   const webpackDevMiddleware = require('./middlewares/webpackDev');
+//   const webpackHotMiddleware = require('./middlewares/webpackHot');
+//
+//   app.use(webpackDevMiddleware);
+//   app.use(webpackHotMiddleware);
+// }
+
+app.use('/assets', express.static('assets'));
+app.get('*', renderInitialPage());
 
 app.listen(config.port, () => {
   console.log('Server is started http://localhost:%s', config.port);
